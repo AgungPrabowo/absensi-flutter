d5e34dbe66243db9509b472fbda33f029eda573a
AgungPrabowo <agungprabowo112@gmail.com>
2020-02-22 15:29:29 +0700

    first conmmit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:absensi/views/homePage.dart';
+import 'package:absensi/views/login.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kodeversitas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        fontFamily: 'Nunito',
+      ),
+      home: LoginPage(),
+      routes: routes,
+    );
+  }
+}
